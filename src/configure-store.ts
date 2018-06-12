ac600ab8e1c35f11cc27e9fbd3ea70f6f52dbc60
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-06-12 23:00:00 +0300

    Ugly typefix...

--- src/configure-store.ts
+++ src/configure-store.ts
@@ -37,7 +37,7 @@ export function configureStore<State>(options: {
 
     const composedEnhancer = finalCompose(...storeEnhancers);
 
-    const store = createStore(reducer, preloadedState, composedEnhancer);
+    const store = createStore(reducer, preloadedState as any, composedEnhancer);
 
     return store;
 }
