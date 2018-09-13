279ba0fcfde15f6c9c1b2a41607f89b9b0b9d267
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-09-13 21:51:21 +0300

    First bits of new thunks

--- __tests__/thunks.test.tsx
+++ __tests__/thunks.test.tsx
@@ -1,4 +1,4 @@
-import {createThunks} from "../src/create-thunks";
+import {makeThunkCreator} from "../src/create-thunks";
 
 import {createSimpleActions, createReducer} from "../src/create-simple-actions";
 import {configureStore} from "../src/configure-store";
@@ -14,78 +14,63 @@ test("thunks work", () => {
         },
     });
 
-    const Thunks = createThunks(SimpleActions, {
-        myThunk(boo: number) {
-            return async dispatch => {
-                // assert return value here too
-                const res: Promise<null> | void = dispatch(
-                    SimpleActions.setFoo({foo: "from thunk"}),
-                );
+    const createThunk = makeThunkCreator(store => ({
+        lol: store.dispatch,
+    }));
 
-                // just for type assertions
-                const fakeDispatch: typeof dispatch = (() => null) as any;
-
-                // These must fail
-
-                // @ts-ignore
-                fakeDispatch({type: "setFoo", payload: {foo: "from thunk"}});
-
-                // @ts-ignore
-                fakeDispatch({});
-
-                // @ts-ignore
-                fakeDispatch();
-            };
-        },
+    const myThunk = createThunk((foo: number) => wot => {
+        wot.lol(SimpleActions.setFoo({foo: "from thunk " + foo}));
     });
 
     const store = configureStore({
         reducer: createReducer(SimpleActions),
     });
 
-    store.dispatch(Thunks.myThunk(3) as any);
+    const ret = myThunk(3);
 
-    expect(store.getState()).toEqual({foo: "from thunk"});
+    store.dispatch(ret);
+
+    expect(store.getState()).toEqual({foo: "from thunk 3"});
 });
 
-test("thunks can call other thunks", async () => {
-    const initialState = {foo: "bar"};
-    const thunkSpy = jest.fn();
+// test("thunks can call other thunks", async () => {
+//     const initialState = {foo: "bar"};
+//     const thunkSpy = jest.fn();
 
-    const SimpleActions = createSimpleActions(initialState, {
-        setFoo(state, action: {foo: string}) {
-            return {...state, foo: action.foo};
-        },
-    });
+//     const SimpleActions = createSimpleActions(initialState, {
+//         setFoo(state, action: {foo: string}) {
+//             return {...state, foo: action.foo};
+//         },
+//     });
 
-    const Thunks = createThunks(SimpleActions, {
-        myThunk(boo: number) {
-            return async (dispatch, getState) => {
-                dispatch(SimpleActions.setFoo({foo: "first"}));
-                thunkSpy();
-                await dispatch(this.slowThunk());
-            };
-        },
+//     const Thunks = createThunks(SimpleActions, {
+//         myThunk(boo: number) {
+//             return async (dispatch, getState) => {
+//                 dispatch(SimpleActions.setFoo({foo: "first"}));
+//                 thunkSpy();
+//                 await dispatch(this.slowThunk());
+//             };
+//         },
 
-        slowThunk() {
-            return async dispatch => {
-                await wait(50);
-                thunkSpy();
-                dispatch(SimpleActions.setFoo({foo: "slow"}));
-            };
-        },
-    });
+//         slowThunk() {
+//             return async dispatch => {
+//                 await wait(50);
+//                 thunkSpy();
+//                 dispatch(SimpleActions.setFoo({foo: "slow"}));
+//             };
+//         },
+//     });
 
-    const store = configureStore({
-        reducer: createReducer(SimpleActions),
-    });
-    store.dispatch(Thunks.myThunk(3));
+//     const store = configureStore({
+//         reducer: createReducer(SimpleActions),
+//     });
+//     store.dispatch(Thunks.myThunk(3));
 
-    expect(thunkSpy).toHaveBeenCalledTimes(1);
-    expect(store.getState()).toEqual({foo: "first"});
+//     expect(thunkSpy).toHaveBeenCalledTimes(1);
+//     expect(store.getState()).toEqual({foo: "first"});
 
-    await wait(100);
+//     await wait(100);
 
-    expect(thunkSpy).toHaveBeenCalledTimes(2);
-    expect(store.getState()).toEqual({foo: "slow"});
-});
+//     expect(thunkSpy).toHaveBeenCalledTimes(2);
+//     expect(store.getState()).toEqual({foo: "slow"});
+// });
--- src/create-thunks.ts
+++ src/create-thunks.ts
@@ -4,29 +4,46 @@ import {
     SimpleActionsMeta,
 } from "./create-simple-actions";
 
-export interface Thunk<State, ActionTypes> {
-    (
-        dispatch: (
-            action: ActionTypes | Thunk<State, ActionTypes>,
-        ) => Promise<null> | void,
-        getState: () => State,
-    ): any;
+interface Thunk {
+    (dispatch: Dispatch, getState: GetState): void;
 }
 
-/**
- * Create thunk actions for side effects etc.
- *
- * @param actions actions object returned by createSimpleActions()
- * @param thunks
- */
-export function createThunks<
-    State,
-    Actions extends SimpleActionsObject<State>,
-    ThunkActions extends {
-        [thunk: string]: (
-            ...args: any[]
-        ) => Thunk<State, ActionTypesFromSimpleActions<Actions>>;
+interface Action {
+    type: string;
+}
+
+interface Dispatch {
+    (action: Action | Thunk): Promise<null> | void;
+}
+
+interface GetState {
+    (): any;
+}
+
+interface SimpleStore {
+    getState: GetState;
+    dispatch: Dispatch;
+}
+
+interface JustThunk {
+    (...args: any[]): void;
+}
+
+export function makeThunkCreator<MappedStore>(
+    mapStore: (store: SimpleStore) => MappedStore,
+) {
+    function createThunk<ThunkArg>(
+        thunk: (arg: ThunkArg) => (arg: MappedStore) => void,
+    ): (arg: ThunkArg) => JustThunk {
+        function myThunk(arg: ThunkArg) {
+            return (dispatch: any, getState: any) => {
+                const mapped = mapStore({dispatch, getState});
+                return thunk(arg)(mapped);
+            };
+        }
+
+        return myThunk;
     }
->(actions: SimpleActionsMeta<State, Actions>, thunks: ThunkActions) {
-    return thunks;
+
+    return createThunk;
 }
