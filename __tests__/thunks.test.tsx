963ea13b776352b083f057f73f326c6b6a2178f3
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-09-13 23:22:29 +0300

    Infer return types

--- __tests__/thunks.test.tsx
+++ __tests__/thunks.test.tsx
@@ -81,3 +81,54 @@ test("thunks can call other thunks", async () => {
     expect(thunkSpy).toHaveBeenCalledTimes(2);
     expect(store.getState()).toEqual({foo: "slow"});
 });
+
+test("thunk dispatch returns correct types", async () => {
+    const initialState = {foo: "bar"};
+    const thunkSpy = jest.fn();
+
+    const SimpleActions = createSimpleActions(initialState, {
+        simple(state, action: {foo: string}) {
+            return {...state, foo: action.foo};
+        },
+    });
+
+    const createThunk = makeThunkCreator(store => ({
+        dispatch: store.dispatch,
+    }));
+
+    const aThunk = createThunk(() => {
+        return store => {
+            store.dispatch(SimpleActions.simple({foo: "slow"}));
+        };
+    });
+
+    const asyncThunk = createThunk(() => {
+        return async store => {
+            await wait(1);
+        };
+    });
+
+    const testThunk = createThunk(() => {
+        return async store => {
+            const simpleRet: void = store.dispatch(
+                SimpleActions.simple({foo: "dsf"}),
+            );
+
+            const thunkRet: void = store.dispatch(aThunk());
+            const thunkRet2 = store.dispatch(aThunk());
+
+            const asyncRet: Promise<void> = store.dispatch(asyncThunk());
+            const asyncRet2 = store.dispatch(asyncThunk());
+
+            thunkSpy();
+        };
+    });
+
+    const store = configureStore({
+        reducer: createReducer(SimpleActions),
+    });
+
+    store.dispatch(testThunk());
+    await wait(10);
+    expect(thunkSpy).toHaveBeenCalledTimes(1);
+});
--- src/create-thunks.ts
+++ src/create-thunks.ts
@@ -13,32 +13,60 @@ interface Action {
 }
 
 interface Dispatch {
-    (action: Action | Thunk): Promise<null> | void;
+    (action: Action | Thunk): void;
 }
 
 interface GetState {
     (): unknown;
 }
 
-interface SimpleStore {
-    getState: GetState;
-    dispatch: Dispatch;
+// interface SimpleStore {
+//     getState: GetState;
+//     dispatch: Dispatch;
+// }
+
+interface JustThunk<T extends Promise<any> | void> {
+    (...args: any[]): OnlyPromise<T>;
 }
 
-interface JustThunk {
-    (...args: any[]): void;
+type OnlyPromise<T> = T extends (...args: any[]) => Promise<any>
+    ? Promise<void>
+    : void;
+
+class SimpleStore {
+    _dispatch: any;
+    getState: any;
+
+    constructor(dispatch: any, getState: any) {
+        this._dispatch = dispatch;
+        this.getState = getState;
+        this.dispatch = dispatch.bind(this);
+    }
+
+    dispatch<T extends Function | Action>(action: T): OnlyPromise<T> {
+        const ret = this._dispatch(action);
+        if (typeof ret.then === "function") {
+            return ret;
+        }
+
+        return undefined as any;
+    }
 }
 
 export function makeThunkCreator<MappedStore>(
     mapStore: (store: SimpleStore) => MappedStore,
 ) {
-    function createThunk<ThunkArg extends any[]>(
-        thunk: (...args: ThunkArg) => (arg: MappedStore) => void,
-    ): (...args: ThunkArg) => JustThunk {
+    function createThunk<
+        ThunkArg extends any[],
+        ThunkReturn extends Promise<any> | void
+    >(
+        thunk: (...args: ThunkArg) => (arg: MappedStore) => ThunkReturn,
+    ): (...args: ThunkArg) => (dispatch: any, getState: any) => ThunkReturn {
         function myThunk(...args: ThunkArg) {
             return (dispatch: any, getState: any) => {
-                const mapped = mapStore({dispatch, getState});
-                return thunk(...args)(mapped);
+                const mapped = mapStore(new SimpleStore(dispatch, getState));
+                const wat = thunk(...args)(mapped);
+                return wat;
             };
         }
 
