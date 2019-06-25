53db377c3d8042529c05a9d70dd5664a4d8a1f8c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-06-26 00:18:29 +0300

    Add test for useReducer() integration

---
+++ __tests__/use-reducer-integration.test.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {render, fireEvent, cleanup} from "@testing-library/react";
+import {
+    ImmerReducer,
+    createActionCreators,
+    createReducerFunction,
+} from "../src/immer-reducer";
+
+afterEach(cleanup);
+
+test("can use with React.useReducer()", () => {
+    const initialState = {foo: ""};
+
+    class Reducer extends ImmerReducer<typeof initialState> {
+        setFoo(foo: string) {
+            this.draftState.foo = foo;
+        }
+    }
+
+    const ActionCreators = createActionCreators(Reducer);
+    const reducerFuntion = createReducerFunction(Reducer);
+
+    function Foo() {
+        const [state, dispatch] = React.useReducer(
+            reducerFuntion,
+            initialState,
+        );
+
+        return (
+            <button
+                data-testid="button"
+                onClick={() => {
+                    dispatch(ActionCreators.setFoo("clicked"));
+                }}
+            >
+                {state.foo}
+            </button>
+        );
+    }
+
+    const rtl = render(<Foo />);
+    const button = rtl.getByTestId("button");
+
+    fireEvent.click(button);
+
+    expect(button.innerHTML).toBe("clicked");
+});
